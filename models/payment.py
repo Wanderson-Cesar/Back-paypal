f38cbf30df0c1ffa67f5b0cf4e8674ab64a6c6bd
Wanderson-Cesar <wanderson.xper@gmail.com>
2023-09-18 14:43:40 -0300

    comint atualizacao do back end

--- models/payment.py
+++ models/payment.py
@@ -5,10 +5,10 @@ class Payment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product = db.Column(db.String(100))
     amount = db.Column(db.Float)
-    status = db.Column(db.String(100))  # Alterado para campo de texto
+    status = db.Column(db.String(100))  
     name = db.Column(db.String(100))
     cpf = db.Column(db.String(11))
-    orderNumber = db.Column(db.String(10))  # Adicionado campo orderNumber
+    orderNumber = db.Column(db.String(10)) 
 
     def __init__(self, product, amount, status, name, cpf, orderNumber):
         self.product = product
--- routes/payment_routes.py
+++ routes/payment_routes.py
@@ -2,22 +2,29 @@ from flask import Blueprint, request, jsonify
 from database import db
 from models.payment import Payment
 
+# Cria um Blueprint para rotas relacionadas a pagamentos
 payment_bp = Blueprint('payment', __name__)
 
+# Rota para salvar um pagamento
 @payment_bp.route('/api/payment/save-payment', methods=['POST'])
 def save_payment():
     try:
         data = request.get_json()
         product = data['product']
         amount = data['amount']
-        status = data['status']  # Não é mais boolean, mas sim uma string
+        status = data['status']  
         name = data['customerName']
         cpf = data['customerCPF']
+        
         # Antes de salvar o CPF no banco de dados, remova pontos e hífens e garanta 11 dígitos
         cpf = ''.join(filter(str.isdigit, data['customerCPF']))
         cpf = cpf.zfill(11)
         orderNumber = data['orderNumber']
+        
+        # Cria um novo objeto de pagamento
         new_payment = Payment(product=product, amount=amount, status=status, name=name, cpf=cpf, orderNumber=orderNumber)
+        
+        # Adiciona o novo pagamento ao banco de dados e faz o commit
         db.session.add(new_payment)
         db.session.commit()
 
@@ -27,16 +34,19 @@ def save_payment():
     except Exception as e:
         print(f"Error saving payment: {str(e)}")
         return jsonify({'error': 'An error occurred'}), 500
-    # Sua função save_payment aqui...
 
+# Rota para atualizar o status de um pagamento
 @payment_bp.route('/api/payment/update-status/<orderNumber>', methods=['PUT'])
 def update_payment_status(orderNumber):
     try:
         data = request.get_json()
         new_status = data['status']
 
+        # Encontra o pagamento pelo número do pedido
         payment = Payment.query.filter_by(orderNumber=orderNumber).first()
+        
         if payment:
+            # Atualiza o status do pagamento e faz o commit
             payment.status = new_status
             db.session.commit()
             print(f"Payment status updated to: {new_status}")
@@ -47,9 +57,29 @@ def update_payment_status(orderNumber):
         print(f"Error updating payment status: {str(e)}")
         return jsonify({'error': 'An error occurred'}), 500
 
+# Rota para excluir um pagamento
+@payment_bp.route('/api/payment/delete-payment/<orderNumber>', methods=['DELETE'])
+def delete_payment(orderNumber):
+    try:
+        payment = Payment.query.filter_by(orderNumber=orderNumber).first()
+        
+        if payment:
+            # Exclui o pagamento e faz o commit
+            db.session.delete(payment)
+            db.session.commit()
+            print(f"Payment with Order Number {orderNumber} deleted successfully")
+            return jsonify({'message': f'Payment with Order Number {orderNumber} deleted successfully'}), 200
+        else:
+            return jsonify({'error': 'Payment not found'}), 404
+    except Exception as e:
+        print(f"Error deleting payment: {str(e)}")
+        return jsonify({'error': 'An error occurred'}), 500
+
+# Rota para obter a lista de pagamentos
 @payment_bp.route('/api/payment/get-payments', methods=['GET'])
 def get_payments():
     try:
+        # Consulta todos os pagamentos no banco de dados
         payments = Payment.query.all()
         payment_list = []
         for payment in payments:
@@ -58,12 +88,11 @@ def get_payments():
                 'product': payment.product,
                 'amount': payment.amount,
                 'status': payment.status,
-                'name': payment.name,  # Adicione o campo 'name'
-                'cpf': payment.cpf      # Adicione o campo 'cpf'
+                'name': payment.name, 
+                'cpf': payment.cpf      
             }
             payment_list.append(payment_data)
         return jsonify(payment_list)
     except Exception as e:
         print(f"Error getting payments: {str(e)}")
         return jsonify({'error': 'An error occurred'}), 500
-
